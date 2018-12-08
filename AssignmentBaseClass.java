dd53d9f8cb2de944697cb2a69d88cb091daff2cc
mqureshi52 <mqureshi52@gmail.com>
2018-12-08 13:48:05 -0500

    third commit

--- AssignmentBaseClass.java
+++ AssignmentBaseClass.java
@@ -1,5 +1,23 @@
 package MavenHomework;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AssignmentBaseClass {
+	
+	public WebDriver driver;
+	
+	public void setup() {
+		driver=new ChromeDriver();
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\User.DESKTOP-QQUOJAQ\\eclipse-workspace\\POM\\Driver\\chromedriver.exe");
+	        driver=new ChromeDriver();
+	        driver.manage().window().fullscreen();
+	        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	        driver.get("https://google.com/");
+	    
+	}
 
 }
--- AssignmentOne.java
+++ AssignmentOne.java
@@ -1,5 +1,22 @@
 package MavenHomework;
 
-public class AssignmentOne {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AssignmentOne extends AssignmentBaseClass{
+	
+	@BeforeMethod
+	public void setup() {
+		setup();
+	}
+	
+	@Test
+	public void getTitle() {
+		String title=driver.getTitle();
+		Assert.assertEquals("Google", title);
+	}
 
 }
+	
+	
